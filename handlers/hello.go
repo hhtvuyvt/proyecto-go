bf8d06c2afc17573a2a00ec3ab7d130d6ce3703a
Juan Camilo Aponte Pertuz <japontepertuz@gmail.com>
2025-06-30 22:23:33 -0500

    completion

--- handlers/hello.go
+++ handlers/hello.go
@@ -1,1 +1,10 @@
 package handlers
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>¡Hola desde Go web!</h1><p>Ruta: %s</p>", r.URL.Path)
+}
--- routes/router.go
+++ routes/router.go
@@ -1,1 +1,18 @@
 package routes
+
+import (
+	"net/http"
+
+	"github.com/hhtvuyvt/proyecto-go/handlers"
+)
+
+func Router() http.Handler {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", handlers.HelloHandler)
+
+	fs := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	return mux
+}
